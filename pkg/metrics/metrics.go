4c113e19af80a4c6d6caa6e916eab4dc0b429d36
liuzhen <liuzhen@yunify.com>
2022-06-06 15:14:09 +0800

    feat: update

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -4,7 +4,7 @@ import "github.com/prometheus/client_golang/prometheus"
 
 const (
 	// metrics label.
-	MetricsLabelTenant = "tenant"
+	MetricsLabelTenant = "tenant_id"
 
 	// metrics name.
 	MetricsNameRuleNum = "rule_num"
